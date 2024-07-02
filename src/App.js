04cd8d8875c2b7b6d9f86976758822f8ee819b8f
Makito <dmakito662@gmail.com>
2024-07-02 07:35:03 +0200

    added routers for navigations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router ,Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path=''/>
+      </Routes>
+      </Router>  
     </div>
   );
 }
