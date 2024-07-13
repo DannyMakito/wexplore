959ebb9380a951a8f7c3f93a43555a90bb743e22
Makito <dmakito662@gmail.com>
2024-07-14 00:58:15 +0200

    created routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router ,Route,Routes} from 'react-router-dom';
 import Home from './components/Home';
+import Services from './components/Services';
+import Signup from './components/Signup';
+import Products from './components/Products';
 
 function App() {
   return (
-    <div className="App">
+    
+ <>
     <Router>
       <Navbar />
         
        <Routes>
        <Route index element={<Home />} />
+       <Route path='/services' element={<Services/>} />
+       <Route path='/products' element={<Products />} />
+       <Route path='/sign-up' element={<Signup />} />
        </Routes>
-        
+    
       </Router>  
-    </div>
+ </>
+  
   );
 }
 
