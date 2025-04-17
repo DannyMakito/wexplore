383668eb4b434c354389c2388aae42de3c4cfde5
DannyMakito <72389518+DannyMakito@users.noreply.github.com>
2025-04-18 01:01:30 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>home</div>
+  <div className='' style={{backGroundImage : "url('')"}}>
+          <div>
+              <h1>Explore the world with us </h1>
+              <p>
+                  Discover amazing places at exclusoives deals
+              </p>
+              <button> get started</button>
+          </div>
+      </div>
   )
 }
 
