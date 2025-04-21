a35d64aa61a8be889dd3fce70fa759ca3aa22067
dannymakito <dmakito662@gmail.com>
2025-04-21 21:55:27 +0200

    updated navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -13,10 +13,10 @@ const NavBar = () => {
             <h3 className='text-2xl font-bold'>Wexplore</h3>
             <div className='hidden  md:flex space-x-4 text-sm font-bold items-center'>
                 <Link to="/" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>Home</Link>
-                <Link to="/" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>Gallary</Link>
-                <Link to="/" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>About</Link>
-                <Link to="/" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>Contact</Link>
-               <button className='py-2 px--6 border bd-gray-300'>Login </button>
+                <Link to="/gallary" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>Gallary</Link>
+                <Link to="/about" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>About</Link>
+                <Link to="/contact" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>Contact</Link>
+               <button className='py-2 px-6 border bg-gray-300 rounded'>Login </button>
 
             </div>
             <div className='md:hidden'>
@@ -31,7 +31,7 @@ const NavBar = () => {
             <Link to="/" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>Gallary</Link>
             <Link to="/" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>About</Link>
             <Link to="/" className='px-6 py-2 hover:bg-gray-600 hover:text-white'>Contact</Link>
-           <button className='py-2 px--6 border bd-gray-300'>Login </button>
+           <button className='py-2 px-6 border bg-gray-600 rounded'> Login </button>
 
         </div>
         )}
