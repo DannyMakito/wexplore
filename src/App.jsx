6a3b83adc2409f3f2f6e524551bf1cd55063dd06
dannymakito <dmakito662@gmail.com>
2025-04-21 21:53:59 +0200

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Home from './pages/home';
 import NavBar from './component/NavBar';
+import Gallary from './pages/Gallary';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Footer from './component/Footer';
 
 function App() {
 
@@ -13,8 +17,11 @@ function App() {
   <NavBar/>
   <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/gallary" element={<Gallary />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      <Route path="/about" element={<About />}></Route>
    </Routes>
-
+    <Footer />
  </BrowserRouter>
      
   
